eb56b895b90978fd128ce33cf35c8f9ed67840ea
ChocolateOwO <152591395+ChocolateOwO@users.noreply.github.com>
2025-01-29 16:06:04 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 
 public class Main {
@@ -40,6 +42,52 @@ public class Main {
         sSet.add("ZZZ");
         sSet.add("123");
         System.out.println(sSet);
+
+        Stack<String> stack = new Stack<>();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        System.out.println("Stack: " + stack);
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack after pop: " + stack);
+
+        Map<String, Color> favoriteColors = new HashMap<String, Color>();
+        favoriteColors.put("Juliet", Color.BLUE);
+        favoriteColors.put("Romeo", Color.GREEN);
+        favoriteColors.put("Adam", Color.RED);
+        favoriteColors.put("Eve", Color.BLUE);
+        favoriteColors.put("Robert", Color.BLACK);
+        favoriteColors.put("Mark", Color.WHITE);
+        favoriteColors.put("Pedro", Color.YELLOW);
+        favoriteColors.put("Cloe", Color.BLUE);
+
+        // Print all keys and values in the map
+
+        Set<String> keySet = favoriteColors.keySet();
+        for (String key : keySet){
+
+            Color value = favoriteColors.get(key);
+        System.out.println(key + " : " + value);
+        }
+
+        Map<String, Color> Mycolor = new TreeMap<String, Color>();
+       Mycolor.put("Juliet", Color.BLUE);
+       Mycolor.put("Romeo", Color.GREEN);
+       Mycolor.put("Adam", Color.RED);
+       Mycolor.put("Eve", Color.BLUE);
+       Mycolor.put("Robert", Color.BLACK);
+       Mycolor.put("Mark", Color.WHITE);
+       Mycolor.put("Pedro", Color.YELLOW);
+       Mycolor.put("Cloe", Color.BLUE);
+
+        Set<String> color = Mycolor.keySet();
+        for (String key : color){
+
+            Color value = Mycolor.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+
 
 
     }
